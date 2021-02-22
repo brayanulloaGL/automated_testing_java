acc225b3e720e865ab8f5407c8bed2a546bce36f
BryanUlloa <43707976+BryanU10@users.noreply.github.com>
2021-02-22 12:06:28 -0600

    Updates

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,6 +1,8 @@
 package tests;
 
+import base.BasePages;
 import base.BaseTests;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
@@ -21,28 +23,36 @@ public class LoginTests extends BaseTests {
     /**
      * This test case verifies the "LOGIN" with locked credentials
      */
-    @Test(groups = {"Login", "Negative" })
-    public void lockedCredentials(){
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.setUsername("wrong_user");
-        loginPage.setPassword("secret_sauce");
-        loginPage.clickLoginButton();
-        assertTrue(loginPage.getErrorMessageText()
-                        .contains("Epic sadface: Sorry, this user has been locked out."),
-                "Credentials are not locked!");
-    }
 
-    /**
-     * This test case verifies the "LOGIN" with invalid credentials
-     */
-    @Test(groups = {"Login", "Negative" })
-    public void invalidCredentials(){
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.setUsername("wrong_user");
-        loginPage.setPassword("wrong_pass");
-        loginPage.clickLoginButton();
-        assertTrue(loginPage.getErrorMessageText()
-                        .contains("Epic sadface: Username and password do not match any user in this service"),
-                "The credentials entered are valid!");
+    public static class Other extends BasePages {
+
+        public Other(WebDriver driver) {
+            super(driver);
+        }
+
+        @Test(groups = {"Login", "Negative" })
+        public void lockedCredentials(){
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.setUsername("wrong_user");
+            loginPage.setPassword("secret_sauce");
+            loginPage.clickLoginButton();
+            assertTrue(loginPage.getErrorMessageText()
+                            .contains("Epic sadface: Sorry, this user has been locked out."),
+                    "Credentials are not locked!");
+        }
+
+        /**
+         * This test case verifies the "LOGIN" with invalid credentials
+         */
+        @Test(groups = {"Login", "Negative" })
+        public void invalidCredentials(){
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.setUsername("wrong_user");
+            loginPage.setPassword("wrong_pass");
+            loginPage.clickLoginButton();
+            assertTrue(loginPage.getErrorMessageText()
+                            .contains("Epic sadface: Username and password do not match any user in this service"),
+                    "The credentials entered are valid!");
+        }
     }
 }
