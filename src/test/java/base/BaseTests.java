276be2953ac6be1a434656a3c3e22f36ec52db77
BryanUlloa <43707976+BryanU10@users.noreply.github.com>
2021-03-02 13:26:13 -0600

    Adding screenshot method code and dependsOn tag

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,14 +1,20 @@
 package base;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.InventoryPage;
 import pages.LoginPage;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
+
+import static org.apache.commons.io.FileUtils.copyFile;
 
 public class BaseTests {
 
@@ -52,10 +58,24 @@ public class BaseTests {
                         .clickLoginButton());
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterMethod(alwaysRun = true, dependsOnMethods = "screenShot")
     public void logout() {
         this.getInventoryPage().clickMenuButton();
         this.getInventoryPage().clickLogoutButton();
+    }
+
+    @AfterMethod()
+    public void screenShot(ITestResult result){
+        if (ITestResult.FAILURE == result.getStatus()){
+            try{
+                TakesScreenshot screenshot = (TakesScreenshot)driver;
+                File src = screenshot.getScreenshotAs(OutputType.FILE);
+                copyFile(src, new File("resources/screenshots/" + result.getName()+ ".png"));
+                System.out.println("Successfully captured a screenshot");
+            }catch (Exception e){
+                System.out.println("Exception while taking screenshot " + e.getMessage());
+            }
+        }
     }
 
     @AfterClass(alwaysRun = true)
