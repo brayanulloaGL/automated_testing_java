a171baf59530d7bf5e7aed878a0f68abc9799b60
BryanUlloa <43707976+BryanU10@users.noreply.github.com>
2021-03-03 17:25:00 -0600

    Log4j implementation

--- src/test/java/tests/login/LoginTestsPositive.java
+++ src/test/java/tests/login/LoginTestsPositive.java
@@ -1,19 +1,29 @@
 package tests.login;
 
 import base.BaseTests;
+
 import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static org.testng.Assert.*;
 
 public class LoginTestsPositive extends BaseTests {
 
+    Logger logger = LogManager.getLogger(LoginTestsPositive.class);
+
+
     /**
      * This test case verifies the "LOGIN" with valid credentials
      */
-        @Test(groups = {"Regression", "Positive"})
+    @Test(groups = {"Regression", "Positive"})
     public void validCredentials() {
+
         assertTrue(this.getInventoryPage().getInventoryContainerText()
                         .contains("Products"),
                 "The text from the inventory container is incorrect!");
+        logger.info("We've just greeted the user!");
+
+
     }
 }
